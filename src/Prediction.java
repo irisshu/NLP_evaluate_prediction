1d7919a97e01924039682882d7595956a29b0ea4
Iris Hsu <irishsuyt@gmail.com>
2014-11-20 18:13:26 +0800

    Success to calculate precision and recall.

--- src/Prediction.java
+++ src/Prediction.java
@@ -8,36 +8,44 @@ public class Prediction {
 	static String gold_cate[] = new String[10000];
 	static String test_cate[] = new String[10000];
 
-	static int tp_book = 0;
-	static int fp_book = 0;
-	static int tn_book = 0;
-	static int fn_book = 0;
+	static double tp_book = 0;
+	static double fp_book = 0;
+	static double tn_book = 0;
+	static double fn_book = 0;
 
-	static int tp_dvd = 0;
-	static int fp_dvd = 0;
-	static int tn_dvd = 0;
-	static int fn_dvd = 0;
+	static double tp_dvd = 0;
+	static double fp_dvd = 0;
+	static double tn_dvd = 0;
+	static double fn_dvd = 0;
 
-	static int tp_health = 0;
-	static int fp_health = 0;
-	static int tn_health = 0;
-	static int fn_health = 0;
+	static double tp_health = 0;
+	static double fp_health = 0;
+	static double tn_health = 0;
+	static double fn_health = 0;
 
-	static int tp_music = 0;
-	static int fp_music = 0;
-	static int tn_music = 0;
-	static int fn_music = 0;
+	static double tp_music = 0;
+	static double fp_music = 0;
+	static double tn_music = 0;
+	static double fn_music = 0;
 
-	static int tp_toys_games = 0;
-	static int fp_toys_games = 0;
-	static int tn_toys_games = 0;
-	static int fn_toys_games = 0;
+	static double tp_toys_games = 0;
+	static double fp_toys_games = 0;
+	static double tn_toys_games = 0;
+	static double fn_toys_games = 0;
+	
+	static Double precision[] = new Double[5]; //oder : book, dvd, health, music, toys_games
+	static Double recall[] = new Double[5];
+	static String category[] = {"book", "dvd", "health", "music", "toys_games"};
 
 	public static void main(String[] args) {
 
+		System.out.println("Start gold_standard.xml reading... ");
 		getData("gold_standard.xml");
+		System.out.println("Start test_outcome.xml reading... ");
 		getData("test_outcome.xml");
 		Process();
+		cal_precision();
+		cal_recall();
 		
 		System.out.println("book-> tp_book,fp_book,tn_book,fn_book : "
 				+ tp_book + "," + fp_book + "," + tn_book + "," + fn_book);
@@ -53,9 +61,44 @@ public class Prediction {
 		
 		System.out.println("toys_games-> tp_toys_games,fp_toys_games,tn_toys_games,fn_toys_games : "
 				+ tp_toys_games + "," + fp_toys_games + "," + tn_toys_games + "," + fn_toys_games);
-
+		print_result();
+		
 	}
+	
+	private static void print_result() {
+		
+		for(int i=0; i<5 ;i++){
+			
+			System.out.println(category[i]);
+			System.out.println("\t p : "+ precision[i]);
+			System.out.println("\t r : "+ recall[i]);
+		}
+	}
+	
+	
 
+	private static void cal_precision() {
+		
+		precision[0]= tp_book/(tp_book+fp_book);
+		precision[1]= (double) (tp_dvd/(tp_dvd+fp_dvd));
+		precision[2]= (double) (tp_health/(tp_health + fp_health));
+		precision[3]= (double) (tp_music/(tp_music+fp_music));
+		precision[4]= (double) (tp_toys_games/(tp_toys_games+fp_toys_games));
+		
+	}
+	
+	private static void cal_recall() {
+		
+		recall[0]= (double) (tp_book/(tp_book+fn_book));
+		recall[1]= (double) (tp_dvd/(tp_dvd+fn_dvd));
+		recall[2]= (double) (tp_health/(tp_health+fn_health));
+		recall[3]= (double) (tp_music/(tp_music+fn_music));
+		recall[4]= (double) (tp_toys_games/(tp_toys_games+fn_toys_games));
+	}
+	
+	
+	
+	
 	private static void Process() {
 
 		for (int i = 0; i < gold_cate.length; i++) {
@@ -289,24 +332,23 @@ public class Prediction {
 		int count = 0;
 
 		if (xml_name.equals("gold_standard.xml")) {
-			System.out.println("Start gold_standard.xml reading... ");
+			
 			for (int i = 0; i < parts.length; i++) {
 				if (parts[i].equals("<Text")) {
 					cutString = parts[i + 1].split("\"");// category=cutString[1]
 					gold_cate[count] = cutString[1];
-					System.out.println("gold : " + gold_cate[count]);
+//					System.out.println("gold : " + gold_cate[count]);
 					count++;
 				}
 			}
 			System.out.println("Finish gold_standard.xml reading... ");
 		} else if (xml_name.equals("test_outcome.xml")) {
 
-			System.out.println("Start test_outcome.xml reading... ");
 			for (int i = 0; i < parts.length; i++) {
 				if (parts[i].equals("<Text")) {
 					cutString = parts[i + 1].split("\"");// category=cutString[1]
 					test_cate[count] = cutString[1];
-					System.out.println("test : " + test_cate[count]);
+//					System.out.println("test : " + test_cate[count]);
 					count++;
 				}
 			}
